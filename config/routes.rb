4616ecf62c8659e62fe910bc6ac3f737a8b815cc
saiki shono <shono131103@gmail.com>
2025-02-16 16:29:48 +0900

    setting sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get "sessions/new"
-  get "sessions/create"
-  get "sessions/destroy"
-  get "users/new"
-  get "users/create"
-  get "users/show"
+  #セッションのルーティング
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  #ユーザーのルーティング
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
   resources :users, only: [:show]
+  
 end
