fc928d20281a45ee0970b1bf397330609b4e78ea
saiki shono <shono131103@gmail.com>
2025-02-16 16:31:20 +0900

    add session controller method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "ログインしました！"
+    else
+      flash.now[:alert] = "メールアドレスまたはパスワードが無効です"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to login_path, notice: "ログアウトしました"
   end
 end
